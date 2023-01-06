54384b92274c0908961b106a9b1fb6d56dc9ec39
prashanth <prashanthpasham@users.noreply.github.com>
2023-01-06 16:56:36 +0530

    Create Singleton.java

---
+++ Singleton.java
@@ -0,0 +1,52 @@
+package com.mq;
+
+import java.io.ObjectStreamException;
+import java.io.Serializable;
+
+public final class Singleton implements Cloneable, Serializable {
+	private volatile static Singleton singleton = null;
+
+	private Singleton() {
+		if (singleton != null) {
+			throw new InstantiationError("object creation is not allowed!");
+		}
+	}
+
+	public static synchronized Singleton getInstance() {
+		if (singleton == null)
+			singleton = new Singleton();
+
+		return singleton;
+	}
+
+	public Object clone() throws CloneNotSupportedException {
+		// return Singleton.getInstance();
+		throw new CloneNotSupportedException();
+	}
+
+	protected Object readResolve() throws ObjectStreamException {
+		return singleton;
+	}
+
+	public static void main(String[] args) {
+
+		try {
+			Singleton ut = Singleton.getInstance();
+			Singleton ut2 = (Singleton) ut.clone();
+			Singleton dt = ut.getClass().newInstance();
+			System.out.println(ut == dt);
+			System.out.println(ut.hashCode +"::"+dt.hashCode());
+
+		} catch (CloneNotSupportedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
